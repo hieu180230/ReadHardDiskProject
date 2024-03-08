de7a9b021f8c1d19fa142cc7b7796b2985f02734
Cookie <hieu180230@gmail.com>
2024-03-08 23:55:24 +0700

    oke xong viewing file tree and fixed minor bugs

--- ReadHardDiskProject/ReadInfo.h
+++ ReadHardDiskProject/ReadInfo.h
@@ -9,7 +9,7 @@ class ReadInfo
 {
 public:
 	static FAT32BootSectorInfo* GetBootSectorInfo(BYTE sector[512]);
-	static void getSDETInfo(LPCWSTR drive, int root_cluster, int sectors_per_cluster, std::string path);
-	static void getRDETInfo(LPCWSTR drive, int root_cluster, int sectors_per_cluster);
+	static void getSDETInfo(LPCWSTR drive, int root_cluster, int sectors_per_cluster, int first_data_sector, std::string path, int level);
+	static void getRDETInfo(LPCWSTR drive, int root_cluster, int sectors_per_cluster, int first_data_sector);
 };
 
